94dcb0036e4f0c8ea918aff34a57c7bc01f957fd
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-25 19:23:57 -0800

    Day 1, part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -15,22 +15,39 @@ fn main() {
         .unwrap()
         .trim()
         .split(", ")
-        .fold(((0_i32, 1_i32), (0, 0)), |acc, direction| {
-            let facing = acc.0;
-            let facing = match &direction[0..1] {
-                "R" => (facing.1, -facing.0),
-                "L" => (-facing.1, facing.0),
-                _ => panic!("Unexpected direction"),
-            };
-            let movement: i32 = direction[1..].parse().unwrap();
+        .fold(
+            ((0_i32, 1_i32), (0, 0), (Vec::new(), None)),
+            |acc, direction| {
+                let facing = acc.0;
+                let facing = match &direction[0..1] {
+                    "R" => (facing.1, -facing.0),
+                    "L" => (-facing.1, facing.0),
+                    _ => panic!("Unexpected direction"),
+                };
+                let movement: i32 = direction[1..].parse().unwrap();
 
-            let new_location = (
-                acc.1 .0 + (movement * facing.0),
-                acc.1 .1 + (movement * facing.1),
-            );
+                let mut new_location = acc.1;
 
-            (facing, new_location)
-        })
+                let mut visited = acc.2 .0.clone();
+                let mut revisited = acc.2 .1;
+                for _ in 0..movement {
+                    new_location.0 += facing.0;
+                    new_location.1 += facing.1;
+                    if revisited.is_none() && visited.contains(&new_location) {
+                        revisited = Some(new_location);
+                        println!(
+                            "Part 2: {}, {} = {}",
+                            new_location.0,
+                            new_location.1,
+                            new_location.0.abs() + new_location.1.abs()
+                        );
+                    }
+                    visited.push(new_location);
+                }
+
+                (facing, new_location, (visited, revisited))
+            },
+        )
         .1;
 
     println!("Part 1: {}", destination.0.abs() + destination.1.abs());
