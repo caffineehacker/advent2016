58072b60f83197ab1dee57f216d39948a5d2c746
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-26 11:27:08 -0800

    Day 3, part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -36,4 +36,37 @@ fn main() {
         .count();
 
     println!("Part 1: {}", valid_count);
+
+    let all_values: Vec<i32> = lines
+        .iter()
+        .flat_map(|line| {
+            line.split_whitespace()
+                .map(|v| v.parse().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+
+    let mut valid_count = 0;
+    for i in 0..3 {
+        valid_count += all_values
+            .iter()
+            .skip(i)
+            .step_by(3)
+            .fold(Vec::new(), |mut acc: Vec<Vec<i32>>, v| {
+                if acc.last().is_none() || acc.last().unwrap().len() == 3 {
+                    acc.push(Vec::new());
+                }
+                acc.last_mut().unwrap().push(*v);
+                acc
+            })
+            .iter()
+            .filter(|sides| {
+                let mut sides = (*sides).clone();
+                sides.sort();
+                sides[0] + sides[1] > sides[2]
+            })
+            .count();
+    }
+
+    println!("Part 2: {}", valid_count);
 }
