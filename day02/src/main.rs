6775bb5ad14989ea94c70fd98f45d4680a38329d
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-26 11:12:40 -0800

    Day 2, part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -21,6 +21,12 @@ fn main() {
         .map(|line| line.expect("Failed to read line"))
         .collect();
 
+    solve_part1(&lines);
+    solve_part2(&lines);
+}
+
+fn solve_part1(lines: &Vec<String>) {
+    print!("Part 1: ");
     let mut last_button = (1, 1);
     for line in lines.iter() {
         for c in line.chars() {
@@ -35,4 +41,48 @@ fn main() {
 
         print!("{}", last_button.0 + 1 + 3 * last_button.1);
     }
+
+    print!("\n");
+}
+
+fn solve_part2(lines: &Vec<String>) {
+    print!("Part 2: ");
+    let mut last_button: (i32, i32) = (0, 2);
+    for line in lines.iter() {
+        for c in line.chars() {
+            match c {
+                'U' => {
+                    last_button.1 = (last_button.1 - 1).max(last_button.0.abs_diff(2_i32) as i32)
+                }
+                'D' => {
+                    last_button.1 =
+                        (last_button.1 + 1).min(4 - last_button.0.abs_diff(2_i32) as i32)
+                }
+                'L' => {
+                    last_button.0 = (last_button.0 - 1).max(last_button.1.abs_diff(2_i32) as i32)
+                }
+                'R' => {
+                    last_button.0 =
+                        (last_button.0 + 1).min(4 - last_button.1.abs_diff(2_i32) as i32)
+                }
+                _ => panic!("Unexpected direction"),
+            }
+        }
+
+        print!(
+            "{}",
+            match last_button {
+                (2, 0) => 1.to_string(),
+                (x, 1) => (x + 1).to_string(),
+                (x, 2) => (x + 5).to_string(),
+                (1, 3) => "A".to_string(),
+                (2, 3) => "B".to_string(),
+                (3, 3) => "C".to_string(),
+                (2, 4) => "D".to_string(),
+                _ => panic!("Unexpected location"),
+            }
+        );
+    }
+
+    print!("\n");
 }
