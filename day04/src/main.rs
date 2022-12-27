21a2c41bcd1c49b89f530f58e54f2da97aa9ca8d
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-26 16:03:26 -0800

    Day 4, part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -23,18 +23,28 @@ fn main() {
         .map(|line| line.expect("Failed to read line"))
         .collect();
 
-    let good_sectors: u32 = lines
-        .iter()
-        .filter_map(|line| {
-            if is_valid(line) {
-                Some(get_sector_id(line))
-            } else {
-                None
-            }
-        })
-        .sum();
+    let good_sectors: Vec<&String> = lines.iter().filter(|line| is_valid(line)).collect();
 
-    println!("Part 1: {}", good_sectors);
+    let sum_of_good_sector_ids: u32 = good_sectors.iter().map(|line| get_sector_id(line)).sum();
+
+    println!("Part 1: {}", sum_of_good_sector_ids);
+
+    good_sectors.iter().for_each(|line| {
+        let mut room_name = get_room_name(line);
+        let sector_id = get_sector_id(line);
+
+        room_name = room_name
+            .chars()
+            .map(|c| {
+                if c == '-' {
+                    return ' ';
+                }
+                ((c as u8 + (sector_id % 26) as u8 - 'a' as u8) % 26 + 'a' as u8) as char
+            })
+            .collect();
+
+        println!("{} ({})", room_name, sector_id);
+    });
 }
 
 fn is_valid(line: &str) -> bool {
@@ -82,4 +92,9 @@ fn get_sector_id(line: &str) -> u32 {
         .unwrap()
         .parse()
         .unwrap()
+}
+
+fn get_room_name(line: &str) -> String {
+    let line: String = line.split_once("[").unwrap().0.chars().rev().collect();
+    line.split_once("-").unwrap().1.chars().rev().collect()
 }
