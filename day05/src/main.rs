e19727b2c681416099df99fd20bdc069eb17ca5b
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-26 21:03:07 -0800

    Day 5, part 2 - with hacker display

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -10,22 +10,51 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let progress = indicatif::ProgressBar::new_spinner();
+    let multiprogress = indicatif::MultiProgress::new();
+    let part_1progress = multiprogress.add(indicatif::ProgressBar::new_spinner());
+    let part_2progress = multiprogress.add(indicatif::ProgressBar::new_spinner());
 
     let mut i = 0;
-    let mut codes_found = "".to_string();
+    let mut part1_codes_found = "".to_string();
+    let mut part2_code: Vec<char> = vec!['_'; 8];
     loop {
-        progress.set_message(format!("Checking {}, found {}", i, codes_found.len()));
+        if part1_codes_found.len() < 8 {
+            part_1progress.set_message(format!(
+                "Part 1: checking {}, found {}",
+                i,
+                part1_codes_found.len()
+            ));
+        }
+        part_2progress.set_message(format!(
+            "Part 2: checking {}, found {}",
+            i,
+            part2_code.iter().collect::<String>()
+        ));
         let hash = format!(
             "{:x}",
             md5::compute(args.door_id.clone() + i.to_string().as_str())
         );
 
         if hash.starts_with("00000") {
-            codes_found += &hash[5..6];
-            if codes_found.len() == 8 {
-                println!("Part 1: {}", codes_found);
-                break;
+            if part1_codes_found.len() < 8 {
+                part1_codes_found += &hash[5..6];
+                if part1_codes_found.len() == 8 {
+                    part_1progress.finish_with_message(format!("Part 1: {}", part1_codes_found));
+                }
+            }
+
+            let index = hash[5..6].parse::<usize>();
+            if let Ok(index) = index {
+                if index < 8 && part2_code[index] == '_' {
+                    part2_code[index] = hash.chars().nth(6).unwrap();
+                    if !part2_code.contains(&'_') {
+                        part_2progress.finish_with_message(format!(
+                            "Part 2: {}",
+                            part2_code.iter().collect::<String>()
+                        ));
+                        break;
+                    }
+                }
             }
         }
 
