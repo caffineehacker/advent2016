aff751e43fc8312a2aff5d705d6d85e59a9fd4c3
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-29 20:50:53 -0800

    Day 14, part 1

---
+++ day14/src/main.rs
@@ -0,0 +1,84 @@
+use std::collections::{HashSet, VecDeque};
+
+use clap::Parser;
+use indicatif::ProgressBar;
+use itertools::Itertools;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    salt: String,
+    #[arg(long)]
+    debug: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let progress = ProgressBar::new_spinner();
+
+    let mut i = 0;
+    let mut seen_triples: VecDeque<(i32, char)> = VecDeque::new();
+    let mut key_indexes = HashSet::new();
+    'hashing: loop {
+        progress.tick();
+        progress.set_message(format!("{}", i));
+        let digest = md5::compute(format!("{}{}", args.salt, i.to_string()));
+        let hashed = format!("{:x}", digest);
+        let (triple, quintuples) = get_repeats(&hashed);
+
+        loop {
+            if !seen_triples.is_empty() && seen_triples.front().unwrap().0 < i - 1000 {
+                seen_triples.pop_front();
+            } else {
+                break;
+            }
+        }
+
+        for quintuple in quintuples {
+            for trip in seen_triples.iter().filter(|trip| trip.1 == quintuple) {
+                key_indexes.insert(trip.0);
+                if key_indexes.len() >= 64
+                    && *key_indexes.iter().sorted().nth(63).unwrap() < i - 1000
+                {
+                    println!("Part 1: {}", key_indexes.iter().sorted().nth(63).unwrap());
+                    break 'hashing;
+                }
+            }
+        }
+
+        if let Some(triple) = triple {
+            //println!("{}: {}", i, hashed);
+            seen_triples.push_back((i, triple));
+        }
+
+        i += 1;
+    }
+}
+
+fn get_repeats(digest: &str) -> (Option<char>, Vec<char>) {
+    let mut last_char = 'Z';
+    let mut last_char_count = 0;
+    let mut triple = None;
+    let mut quintuples = Vec::new();
+    for c in digest.chars() {
+        if c == last_char {
+            last_char_count += 1;
+            if last_char_count == 3 && triple.is_none() {
+                triple = Some(c);
+            }
+            if last_char_count == 5 {
+                quintuples.push(last_char);
+            }
+        } else {
+            last_char = c;
+            last_char_count = 1;
+        }
+    }
+
+    quintuples.sort();
+    quintuples.dedup();
+
+    (triple, quintuples)
+}
